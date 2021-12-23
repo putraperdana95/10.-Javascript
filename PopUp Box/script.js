76ad7f4314f9bc74be0203b2b2ffeceb5cb715a8
putraperdana95 <putra.perdana7@gmail.com>
2021-12-23 20:50:31 +0700

    mengkombinasikan popup box

--- PopUp Box/script.js
+++ PopUp Box/script.js
@@ -1,1 +1,20 @@
-alert("Hello World");
+// var nama = prompt("Masukkan nama:");
+// alert(nama);
+
+// var tes = confirm("yakin ingin melanjutkan ke halaman in?");
+// if (tes === true) {
+//   alert("user menekan tombol yakin");
+// } else {
+//   alert("user tidak bekerja sama");
+// }
+
+alert("Selamat datang..");
+var lagi = true;
+
+while (lagi === true) {
+  var nama = prompt("masukkan nama");
+  alert("halo " + nama);
+
+  lagi = confirm("ingin coba lagi?");
+}
+alert("Terima Kasih Dong");
