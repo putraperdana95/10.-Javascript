86e14656eb0309d1070105bae8e61bae56bf50b1
putraperdana95 <putra.perdana7@gmail.com>
2021-12-24 16:27:37 +0700

    menambah folder for

---
+++ 3. For/script.js
@@ -0,0 +1,11 @@
+var ulang = true;
+while (ulang === true) {
+  console.log("Hello World");
+  ulang = confirm("lagi?");
+}
+
+var nilaiAwal = 1;
+while (nilaiAwal <= 5) {
+  console.log("Hello World" + " " + nilaiAwal + " " + "x");
+  nilaiAwal++;
+}
