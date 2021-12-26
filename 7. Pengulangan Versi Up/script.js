044e87945af7245dd987f32ae0f590fe59fdad24
putraperdana95 <putra.perdana7@gmail.com>
2021-12-26 13:56:36 +0700

    pengulangan terbalik

--- 7. Pengulangan Versi Up/script.js
+++ 7. Pengulangan Versi Up/script.js
@@ -1,6 +1,13 @@
 var s = "";
-for (var i = 0; i < 10; i++) {
-  for (var j = 0; j < 5; j++) {
+// for (var i = 0; i < 7; i++) {
+//   for (var j = 0; j <= i; j++) {
+//     s += "*";
+//   }
+//   s += "\n";
+// }
+// console.log(s);
+for (var i = 7; i > 0; i--) {
+  for (var j = 0; j < i; j++) {
     s += "*";
   }
   s += "\n";
