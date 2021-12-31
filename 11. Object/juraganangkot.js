5e0a964f1550591a8758c65e3d05319afbcc8de6
putraperdana95 <putra.perdana7@gmail.com>
2021-12-31 10:59:56 +0700

    meneyelsaikan latihan terakhir

---
+++ 11. Object/juraganangkot.js
@@ -0,0 +1,30 @@
+// membuat object angkot
+function Angkot(supir, trayek, penumpang, kas) {
+  this.supir = supir;
+  this.trayek = trayek;
+  this.penumpang = penumpang;
+  this.kas = kas;
+
+  this.penumpangNaik = function (namaPenumpang) {
+    this.penumpang.push(namaPenumpang);
+    return this.penumpang;
+  };
+
+  this.penumpangTurun = function (namaPenumpang, bayar) {
+    if (this.penumpang.length === 0) {
+      alert("angkot masih kosong");
+      return false;
+    }
+
+    for (var i = 0; i < this.penumpang.length; i++) {
+      if (this.penumpang[i] == namaPenumpang) {
+        this.penumpang[i] = undefined;
+        this.kas += bayar;
+        return this.penumpang;
+      }
+    }
+  };
+}
+
+var angkot1 = new Angkot("Putra Perdana", ["Kp Jawa", "Selat Bedan"], [], 0);
+var angkot2 = new Angkot("Andika", ["Kp Tengah", "Rawamangun"], [], 0);
